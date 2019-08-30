aa34c3a6c583a6c8b496bbff8362fff4b2e6125f
Daniel <tlam@stevens.edu>
2019-08-30 16:20:22 +0100

    Hello World worked

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import HelloWorld from './HelloWorld';
 function App() {
   return (
     <div className="App">
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <HelloWorld/>
         <a
           className="App-link"
           href="https://reactjs.org"
