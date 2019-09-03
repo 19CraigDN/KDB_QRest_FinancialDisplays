f64c1f89384a04acd69f7db4b4406233305c5ecd
19craigdn <19craigdn@gmail.com>
2019-09-03 11:52:15 +0100

    Added simple aggrid table

--- src/qrest.js
+++ src/qrest.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import axios from 'axios';
-
-
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
 export default class QRest extends React.Component {
     state = {
-        persons: []
+        columnDefs: [],
+        rowData: []
     }
 
     componentDidMount() {
@@ -17,23 +19,36 @@ export default class QRest extends React.Component {
           }
 
         const empty = {
-            "query": "select from t",
+            "query": "select a from t",
             "response": "true",
             "type": "sync"
         };
 
         axios.post(`https://localhost:8090/executeQuery`, empty, config)
         .then(res => {
-            const persons = res.data.result;
-            this.setState({ persons });
+            const rowData = res.data.result;
+            this.setState({ columnDefs:[{
+                headerName: "A", field: "a"
+            }],rowData });
         })
     }
 
     render() {
         return (
-            <ul>
+            <div 
+                className="ag-theme-balham"
+                style={{ 
+                height: '140px', 
+                width: '210px' }} 
+            >
+                <AgGridReact
+                    columnDefs={this.state.columnDefs}
+                    rowData={this.state.rowData}>
+                </AgGridReact>
+            </div>
+            /*<ul>
                 { this.state.persons.map(person => <li>{person.b + person.a}</li>)}
-            </ul>
-        )
+            </ul>*/
+        );
     }
 }
