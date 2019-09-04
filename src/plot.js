9e8196ff87f747148ce47c9044af825c6224acc2
jwrno1 <53181562+jwrno1@users.noreply.github.com>
2019-09-04 09:41:25 +0100

    basic graph showing two plots

--- src/plot.js
+++ src/plot.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import Plot from 'react-plotly.js';
 import Plotly from 'plotly.js-basic-dist';
 import createPlotlyComponent from 'react-plotly.js/factory';
 
@@ -11,7 +10,13 @@ export default class App extends React.Component {
               y: [2, 6, 3],
               mode: 'lines+points',
               marker: {color: 'red'}
-            }],
+            },{
+                type: 'scatter',
+                x: [1, 2, 3],
+                y: [3, 5, 4],
+                mode: 'lines+points',
+                marker: {color: 'blue'}
+              }],
           layout: {width: 320, height: 240, title: 'A Fancy Plot'} 
         }
         Plot = createPlotlyComponent(Plotly); 
