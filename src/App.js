aac18595003bb33da22f8db0808f575982dbda60
Daniel <tlam@stevens.edu>
2019-09-04 11:03:03 +0100

    can display table querying the GW for today

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import QRest from './qrest';
+import QRest_gw from './qrest_gw';
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <QRest/>
+        <QRest_gw/>
         <a
           className="App-link"
           href="https://reactjs.org"
