501d924e1067d74d1a89355646fa47c8131577fc
Matthew <matt.moore@aquaq.co.uk>
2019-09-04 12:37:53 +0100

    pre-josh merge

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import QRest from './qrest';
-import FPlot from './components/CurrentPriceGraph.js';
+import FPlot from './components/LineChartWithScrollZoom.js';
 
 function App() {
   return (
