1da97c7d79e3a8cc4d0a70441b40c72280039959
Matthew <matt.moore@aquaq.co.uk>
2019-09-04 18:06:39 +0100

    graph with gw data and changing color

---
+++ src/components/amtest2.js
@@ -0,0 +1,110 @@
+import React from 'react';
+import '../App.css';
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
+import am4themes_dark from "@amcharts/amcharts4/themes/dark.js";
+import axios from 'axios';
+
+am4core.useTheme(am4themes_dark);
+am4core.useTheme(am4themes_animated);
+
+export default class App extends React.Component { 
+    componentDidMount(){
+        let chart = am4core.create("chartdiv", am4charts.XYChart);
+
+        // Create axes
+        let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+        dateAxis.renderer.minGridDistance = 50;
+
+        let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+        // Create series
+        let series = chart.series.push(new am4charts.LineSeries());
+        series.dataFields.valueY = "visits";
+        series.dataFields.dateX = "date";
+        series.strokeWidth = 2;
+        series.minBulletDistance = 10;
+        series.tooltipText = "{valueY}";
+        series.tooltip.pointerOrientation = "vertical";
+        series.tooltip.background.cornerRadius = 20;
+        series.tooltip.background.fillOpacity = 0.5;
+        series.tooltip.label.padding(12,12,12,12);
+        series.propertyFields.stroke = "color";
+
+        // Add scrollbar
+        chart.scrollbarX = new am4charts.XYChartScrollbar();
+        chart.scrollbarX.series.push(series);
+
+        // Add cursor
+        
+    
+        chart.cursor = new am4charts.XYCursor();
+        chart.cursor.xAxis = dateAxis;
+        chart.cursor.snapToSeries = series;
+
+        let config = {
+            headers: {
+                "Accept": "*/*",
+                "Authorization": "Basic dXNlcjpwYXNz"
+            }
+          }
+
+        const empty = {
+            "query": "0!select avg price, first sym by 0D01:00:00 xbar time from trade where sym = `AAPL, time within (\"p\"$2019.09.02D00:00:00;\"p\"$2019.09.04D23:59:59)",
+            "response": "true",
+            "type": "sync"
+        };
+        var previousValue;
+        var tempcolor = chart.colors.getIndex(0);
+        axios.post(`https://localhost:8090/executeQuery`, empty, config)
+        .then(res => {
+            var gwData = res.data.result;
+            //console.log(gwData);
+            //formats the JSON from qrest to an array of objects
+            let stockChartValuesFunction = [];
+            for (var key in gwData) {
+                if(key > 0){ 
+                    if(previousValue <= gwData[key].price){
+                        tempcolor = chart.colors.getIndex(0);
+                    }
+                    else {
+                        tempcolor = chart.colors.getIndex(5);
+                    };
+                    stockChartValuesFunction[key-1].color = tempcolor;    
+                }
+                
+    
+                stockChartValuesFunction.push({
+                    date: gwData[key].time,
+                    visits: gwData[key].price,
+                });
+                previousValue = gwData[key].price
+            }
+        chart.data = stockChartValuesFunction;
+        console.log(stockChartValuesFunction)
+        
+        
+            
+            // add color to previous data item depending on whether current value is less or more than previous value
+            /*if(key > 0){ 
+                if(previousValue <= visits){
+                    color: chart.colors.getIndex(0);
+                }
+                else {
+                    color: chart.colors.getIndex(5);
+                }
+            }
+*/
+
+
+ 
+        })
+
+        }
+        render() {
+            return (
+            <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>
+            );
+      }
+}
