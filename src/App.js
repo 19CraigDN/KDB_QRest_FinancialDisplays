46c6ef1dc4236eba69fd7ef83559d96a36cf2909
Matthew <matt.moore@aquaq.co.uk>
2019-09-04 11:40:08 +0100

    working graph

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import QRest from './qrest';
-import FPlot from './plot.js';
+import FPlot from './components/CurrentPriceGraph.js';
 
 function App() {
   return (
--- src/components/CurrentPriceGraph.js
+++ src/components/CurrentPriceGraph.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import axios from 'axios';
-{//import Plot from 'react-plotly.js';?}
-import Plotly from 'plotly.js-basic-dist'
-import createPlotlyComponent from 'react-plotly.js/factory'
+import Plotly from 'plotly.js-basic-dist';
+import createPlotlyComponent from 'react-plotly.js/factory';
 
 export default class CurrentPriceGraph extends React.Component {
     constructor(props) {
@@ -27,7 +26,7 @@ export default class CurrentPriceGraph extends React.Component {
           }
 
         const empty = {
-            "query": "0!select avg price, first sym by 1 xbar time.minute from trade where sym = `MSFT, time within (\"p\"$2019.09.02D00:00:00;\"p\"$2019.09.02D23:59:59)",
+            "query": "0!select avg price, first sym by 60 xbar time.minute from trade where sym = `MSFT, time within (\"p\"$2019.09.04D00:00:00;\"p\"$2019.09.04D23:59:59)",
             "response": "true",
             "type": "sync"
         };
@@ -43,14 +42,14 @@ export default class CurrentPriceGraph extends React.Component {
                 stockChartXValuesFunction.push(persons[key].minute.i);
                 stockChartYValuesFunction.push(persons[key].price);
             }
-            console.log(stockChartXValuesFunction)
-            console.log(stockChartYValuesFunction)
+
             pointerToThis.setState({
                 stockChartXValues: stockChartXValuesFunction,
                 stockChartYValues: stockChartYValuesFunction
             });
         })
     }
+    Plot = createPlotlyComponent(Plotly);
 
     render() {
         return (
@@ -58,7 +57,19 @@ export default class CurrentPriceGraph extends React.Component {
                 <h1>Stock Market</h1>
                 <p>x-values: {this.state.stockChartXValues}</p>
                 <p>y-values: {this.state.stockChartYValues}</p>
-                
+                <this.Plot 
+                    data= {[
+                        {
+                        type: 'scatter',
+                        x: this.state.stockChartXValues,
+                        y: this.state.stockChartYValues,
+                        mode: 'lines+points',
+                        marker: {color: 'red'}
+                        }
+                    ]}
+                    layout={{title: 'MSFT'}}
+        />
+      }
             </div>
         )
     }
