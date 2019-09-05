ee5bab586f605807f3431b86f8bdf4d3442aebae
jwrno1 <53181562+jwrno1@users.noreply.github.com>
2019-09-05 17:07:34 +0100

    minor fixes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import QRest from './qrest';
 import Test from './components/amtest2';
-import CPG from './components/CurrentPriceGraph.js'
 
 function App() {
   return (
