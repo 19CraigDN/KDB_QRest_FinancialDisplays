dee770bdedeaa961bb6f59bc54a600c42d8e5548
Daniel <tlam@stevens.edu>
2019-09-05 11:50:21 +0100

    Have two tables in Tab

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './UnknownFinance.PNG';
 import './App.css';
 import QRest_gw_max from './qrest_gw_max';
 import { render } from "react-dom";
+import QRest_gw from './qrest_gw';
 import Tabs from './Tabs';
 require('./styles.css');
 
@@ -16,11 +17,12 @@ function App() {
       <Tabs>
         <div label="Table">
           <QRest_gw_max/>
-          </div>
-          <div label="Something else">
-            After 'while, <em>Croc</em>!
-          </div>
-        </Tabs>
+          <QRest_gw/>
+        </div>
+        <div label="Something else">
+          After 'while, <em>Croc</em>!
+        </div>
+      </Tabs>
     </div>
   );
 }
